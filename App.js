6e6777a1fa50e1c7ce34e7031aadcf6b26c34444
Lucas Santana <132163707+LucasSmaley@users.noreply.github.com>
2024-05-05 17:33:17 +0000

    adicionando tela de login

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, Text, TextInput, View } from 'react-native';
+import { styles } from './styles'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>aaaaaaaaaaaa</Text>
+      <Text style={styles.formTitle}>Login OAASIS</Text>
+      <TextInput style={styles.formInput}
+        placeholder="Informe o E-mail"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoComplete="email"
+      />
+      <TextInput style={styles.formInput}
+        placeholder="Informe a Senha"
+        autoCapitalize="none"
+        secureTextEntry
+      />
+      <Pressable style={styles.formButton}>
+        <Text style={styles.textButton}>Logar</Text>
+      </Pressable>
+      <View style={styles.subContainer}>
+        <Pressable style={styles.subButton}>
+          <Text style={styles.subTextButton}>Esqueci a senha</Text>
+        </Pressable>
+        <Pressable style={styles.subButton}>
+          <Text style={styles.subTextButton}>Novo usuário</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
