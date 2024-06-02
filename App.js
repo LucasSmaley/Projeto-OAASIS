ee4472c0c69e7f2fb6d004ec6dcbcfd6ac3e127f
JeffersonBurn <nascimentojeferson1103@gmail.com>
2024-06-02 17:13:29 -0300

    Esconder StatusBar e melhoria Planejamentos Anual.

--- App.js
+++ App.js
@@ -1,48 +1,55 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './src/Login/Login';
-import CriarLogin from './src/CriarLogin/CriarLogin';
-import Noticias from './src/Noticias/Noticias';
-import TelaInicialPai from './src/TelaInicialPai/TelaInicialPai';
-import PlanejamentoAnual from './src/PlanejamentoAnual/PlanejamentoAnual';
-import SobreNos from './src/About Us/SobreNos';
-import Transparencia from './src/Transparencia/Transparencia';
-import Projetos from './src/Projetos/Projetos';
-import Eventos from './src/Eventos/Eventos';
-import Apoie from './src/Apoie/Apoie';
-import Parceiros from './src/Parceiros/Parceiros';
-import Contato from './src/Contato/Contato';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/Login/Login";
+import CriarLogin from "./src/CriarLogin/CriarLogin";
+import Noticias from "./src/Noticias/Noticias";
+import TelaInicialPai from "./src/TelaInicialPai/TelaInicialPai";
+import PlanejamentoAnual from "./src/PlanejamentoAnual/PlanejamentoAnual";
+import SobreNos from "./src/About Us/SobreNos";
+import Transparencia from "./src/Transparencia/Transparencia";
+import Projetos from "./src/Projetos/Projetos";
+import Eventos from "./src/Eventos/Eventos";
+import Apoie from "./src/Apoie/Apoie";
+import Parceiros from "./src/Parceiros/Parceiros";
+import Contato from "./src/Contato/Contato";
+import { StatusBar } from "expo-status-bar";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login"
-      
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#001d3d',
-          height: 55
-        },
-        headerTintColor: 'white',
-      }}
-      >
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Notificações" component={Noticias} />
-        <Stack.Screen name="Criar Login" component={CriarLogin} />
-        <Stack.Screen name="Home" component={TelaInicialPai}/>
-        <Stack.Screen name="Planejamento Anual" component={PlanejamentoAnual}/>
-        <Stack.Screen name="Sobre Nós" component={SobreNos}/>
-        <Stack.Screen name="Transparencia" component={Transparencia}/>
-        <Stack.Screen name="Projetos" component={Projetos}/>
-        <Stack.Screen name="Eventos" component={Eventos}/>
-        <Stack.Screen name="Apoie" component={Apoie}/>
-        <Stack.Screen name="Parceiros" component={Parceiros}/>
-        <Stack.Screen name="Contato" component={Contato}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar hidden />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#001d3d",
+              height: 55,
+            },
+            headerTintColor: "white",
+          }}
+        >
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Notificações" component={Noticias} />
+          <Stack.Screen name="Criar Login" component={CriarLogin} />
+          <Stack.Screen name="Home" component={TelaInicialPai} />
+          <Stack.Screen
+            name="Planejamento Anual"
+            component={PlanejamentoAnual}
+          />
+          <Stack.Screen name="Sobre Nós" component={SobreNos} />
+          <Stack.Screen name="Transparencia" component={Transparencia} />
+          <Stack.Screen name="Projetos" component={Projetos} />
+          <Stack.Screen name="Eventos" component={Eventos} />
+          <Stack.Screen name="Apoie" component={Apoie} />
+          <Stack.Screen name="Parceiros" component={Parceiros} />
+          <Stack.Screen name="Contato" component={Contato} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
--- src/PlanejamentoAnual/styles.js
+++ src/PlanejamentoAnual/styles.js
@@ -3,81 +3,35 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#cbf3f0",
     alignItems: "center",
     justifyContent: "flex-start",
   },
 
-  formTitle: {
-    fontSize: 36,
-    fontWeight: "bold",
-    color: "blue",
-    margin: 10,
-  },
-
-  formInput: {
-    borderColor: "blue",
-    borderWidth: 1,
-    borderRadius: 10,
-    fontSize: 22,
-    width: "80%",
-    padding: 10,
-    margin: 10,
-  },
-
-  formButton: {
-    backgroundColor: "blue",
-    width: "80%",
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
-    alignItems: "center",
-  },
-
-  textButton: {
-    color: "white",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-
-  subContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "80%",
-  },
-
-  subButton: {
-    padding: 10,
-  },
-
-  subTextButton: {
-    color: "blue",
-  },
-
   monthPlan: {
     width: "90%",
-    backgroundColor: "blue",
+    backgroundColor: "#00bbf9",
     margin: 10,
     padding: 10,
     borderRadius: 10,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
   },
 
   title: {
-    color: 'white',
+    color: "white",
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 3,
   },
 
   subtitle: {
-    color: 'blue',
+    color: "blue",
     fontSize: 13,
     marginBottom: 15,
-    fontWeight: 'bold', 
-    color: '#888', 
+    fontWeight: "bold",
+    color: "#888",
   },
-  
+
   topicContainer: {
     marginBottom: 15,
   },
@@ -87,7 +41,7 @@ export const styles = StyleSheet.create({
   },
 
   topic: {
-    color:'#000',
-    fontWeight: '600',
+    color: "#024059",
+    fontWeight: "600",
   },
 });
